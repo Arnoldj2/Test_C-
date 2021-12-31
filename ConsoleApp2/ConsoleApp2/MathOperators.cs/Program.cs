cd8dcfaceedbdaff085c5c1c29ab1a3ad31c6663
Jeffrey Arnold <74551278+Arnoldj2@users.noreply.github.com>
2021-12-31 16:21:20 -0700

    test
    test

---
+++ ConsoleApp2/ConsoleApp2/MathOperators.cs/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperators.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 10 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTempurature = 70;
+            //int currentTempurature = 70;
+
+            ////bool isWarm = currentTempurature <= roomTempurature;
+            //bool isWarm = currentTempurature != roomTempurature;
+
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate1 = Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            string hours1 = Console.ReadLine();
+            int salary1 = Convert.ToInt32(hourlyRate1) * Convert.ToInt32(hours1) * 52;
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate2 = Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            string hours2 = Console.ReadLine();
+            int salary2 = Convert.ToInt32(hourlyRate2) * Convert.ToInt32(hours2) * 52;
+
+            Console.WriteLine("annual salary of person 1: ");
+            Console.WriteLine(salary1);
+            Console.WriteLine("annual salary of person 2: ");
+            Console.WriteLine(salary2);
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool isMore = salary1 > salary2;
+            Console.WriteLine(isMore);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Age?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Y or N");
+            string Answer = Console.ReadLine().ToLower();
+            bool DUI = Answer == "n";
+            Console.WriteLine("How many speeding tickets do you have?");
+            int Speeding = Convert.ToInt32(Console.ReadLine());
+
+            bool canCover = (Age >= 15 && DUI == true && Speeding < 3);
+            Console.WriteLine(canCover);
+            Console.ReadLine();
+        }
+    }
+}
